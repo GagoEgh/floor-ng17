d8e2d3e24f80db41e01804583e71de90b0d5559b
unknown <egagik69@gmail.com>
2024-02-14 02:03:09 +0400

    create pagination

--- src/app/components/katalogGroup/katalog/katalog.component.ts
+++ src/app/components/katalogGroup/katalog/katalog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Signal, inject } from '@angular/core';
+import { Component, OnInit, Signal, WritableSignal, computed, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ICovrolin } from '../../../types/covfolin.interface';
 import { MenuService } from '../../../apis/menu.service';
@@ -15,23 +15,61 @@ import { CovrolinComponent } from '../covrolin/covrolin.component';
   styleUrl: './katalog.component.scss'
 })
 export class KatalogKovrolinComponent implements OnInit {
-  title = ''
-  covrolins!: Signal<ICovrolin[] | undefined>
-  menuService = inject(MenuService);
 
+  covrolins!: Signal<ICovrolin[] | undefined>;
+  vewCovrolins!: Signal<ICovrolin[] | undefined>
+  menuService = inject(MenuService);
+  pageCount!: Signal<Array<any>>;
+  pageination: WritableSignal<Pageination> = signal({ page: 0 });
 
   constructor() {
     this.menuService.covrolinsDispatch();
-    this.covrolins = this.menuService.getCovrolinsSignal()
-
+    this.covrolins = this.menuService.getCovrolinsSignal();
 
   }
 
+  private pageLength(): number {
+    return Math.ceil(this.covrolins()!.length / 10);
+  }
 
 
   ngOnInit(): void {
+    this.vewCovrolins = this.covrolins;
+    this.pageCount = signal(new Array(this.pageLength()));
+    this.goToNumberPage(1);
+  }
+
+  updatePage(index: number) {
+    this.pageination.update((p: Pageination) => ({ page: index }));
+  }
+
+  goToNumberPage(index: number) {
+    this.updatePage(index)
+    let size = index * 10;
+    let start = size - 10;
+    this.vewCovrolins = computed(() => this.covrolins()?.slice(start, size));
+  }
+
+  previous() {
+    this.pageination().page--;
+    if(this.pageination().page===0){
+      this.pageination().page = this.pageLength()
+    }
+    this.goToNumberPage(this.pageination().page)
 
   }
 
+  next() {
+    this.pageination().page++;
+    if(this.pageination().page>this.pageLength()){
+      this.pageination().page = 1
+    }
+    this.goToNumberPage(this.pageination().page);
+    console.log('previous', this.pageination())
+  }
 
+}
+
+interface Pageination {
+  page: number
 }
